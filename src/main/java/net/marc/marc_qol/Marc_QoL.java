ea5a84ac171f0d15b68becb016cac968cbca5c48
MarcAnt0z <marcantoinedheur@gmail.com>
2025-04-22 13:07:04 -0400

    added the flower bouquet

--- src/main/java/net/marc/marc_qol/Marc_QoL.java
+++ src/main/java/net/marc/marc_qol/Marc_QoL.java
@@ -1,6 +1,8 @@
 package net.marc.marc_qol;
 
 import com.mojang.logging.LogUtils;
+import net.marc.marc_qol.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -22,6 +24,9 @@ public class Marc_QoL {
     public Marc_QoL() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
@@ -34,7 +39,9 @@ public class Marc_QoL {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.BOUQUET);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
